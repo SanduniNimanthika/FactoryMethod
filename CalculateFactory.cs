4af3523444edea078e42166de78995fa4c672811
SanduniNimanthika <thslsanduni1997@gmail.com>
2021-01-08 12:19:08 +0530

    first commit

---
+++ CalculateFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+  public  class CalculateFactory
+    {
+		public ICalculate GetCalculate(String type)
+		{
+			ICalculate obj = null;
+
+			if (type.ToLower().Equals("add"))
+			{
+				obj = new Add();
+			}
+			else if (type.ToLower().Equals("divide"))
+			{
+				obj = new Divide();
+			}
+			else
+			{
+				Console.WriteLine("no calculation");
+
+			}
+			return obj;
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+	class Test
+	{
+		static void Main()
+		{
+			Console.WriteLine("enter first number");
+			string input = Console.ReadLine();
+			double num1, num2;
+			bool result = Double.TryParse(input, out num1);
+			if (!result)
+			{
+				Console.WriteLine("enter a number");
+				return;
+			}
+			Console.WriteLine("enter second number");
+			
+			 result = Double.TryParse(Console.ReadLine(), out num2);
+			if (!result)
+			{
+				Console.WriteLine("enter a number");
+				return;
+			}
+			Console.WriteLine("Enter Add,Divide");
+		CalculateFactory factory = new CalculateFactory();
+			ICalculate obj= factory.GetCalculate(Console.ReadLine());
+			obj.Calculate(num1, num2);
+			Main();
+
+		}
+	}
+}
